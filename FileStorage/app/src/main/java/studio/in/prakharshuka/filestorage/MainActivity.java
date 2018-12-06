f8ad67b2ba09c1482900f3abf88f4cb853fc010a
Prakhar <prakharshukla@techriz.com>
2018-12-06 11:50:51 +0530

    This repo is all about showing how to download files from firebase storage.

---
+++ FileStorage/app/src/main/java/studio/in/prakharshuka/filestorage/MainActivity.java
@@ -0,0 +1,66 @@
+package studio.in.prakharshuka.filestorage;
+
+import android.app.DownloadManager;
+import android.content.Context;
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+
+import static android.os.Environment.DIRECTORY_DOWNLOADS;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button down;
+    FirebaseStorage firebaseStorage;
+    StorageReference storageReference;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        down=findViewById(R.id.down);
+        down.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                download();
+
+            }
+        });
+    }
+
+    public void download()
+    {
+        storageReference= FirebaseStorage.getInstance().getReference();
+        StorageReference ref=storageReference.child("mobile.pdf");
+
+        ref.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+            @Override
+            public void onSuccess(Uri uri) {
+                String url=uri.toString();
+                 downloadFile(MainActivity.this,"Mobile",".pdf",DIRECTORY_DOWNLOADS,url);
+
+            }
+        });
+    }
+
+    public void downloadFile(Context context, String fileName, String fileExtension, String destinationDirectory, String url) {
+
+        DownloadManager downloadmanager = (DownloadManager) context.
+                getSystemService(Context.DOWNLOAD_SERVICE);
+        Uri uri = Uri.parse(url);
+        DownloadManager.Request request = new DownloadManager.Request(uri);
+
+        request.setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED);
+        request.setDestinationInExternalFilesDir(context, destinationDirectory, fileName + fileExtension);
+
+        downloadmanager.enqueue(request);
+    }
+}
